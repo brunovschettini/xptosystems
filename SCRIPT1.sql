329a1552179a63985ce4cfa4ed77c84252b4e1d4
Bruno Schettini <brunovschettini@outlook.com>
2019-03-12 16:12:40 -0300

    Scripts SQL

---
+++ SCRIPT1.sql
@@ -0,0 +1,110 @@
+-- Folha de respostas
+-- Nome: Bruno Vieira Schettini da Silva
+-- Data: 11/03/2019
+-- Parte 3 – PL/SQL Prático
+
+/*
+Exercício 1.
+Crie as seguintes tabelas, em um script SQL com o nome de SCRIPT1.SQL:
+
+EXAME_NF
+IDNF	NUMERO
+NUMERO	NUMERO
+DATACADASTRO	DATA
+TOTALGERAL	NUMERO
+
+EXAME_ITEMNF
+IDITEMNF	NUMERO
+IDNF	NUMERO
+IDPRODUTO	NUMERO
+QTDE	NUMERO
+VALOR	NUMERO
+
+*/
+
+-- PRODUTOS
+-- DROP TABLE PRODUTO;
+create table PRODUTO (
+   IDPRODUTO number not null constraint produto_pk primary key,
+   CODIGOINTERNO varchar2(60) not null,
+   DESCR varchar2(120) not null,
+   ATIVO char(1) DEFAULT 'S' not null
+); 
+
+
+-- DROP TABLE EMBALAGEM;
+
+create table EMBALAGEM (
+   IDPRODUTO number not null,
+   BARRA varchar2(32) not null,
+   DESCR varchar2(80) not null,
+   FATORCONVERSAO number not null,
+   ALTURA number DEFAULT 0,
+   LARGURA number DEFAULT 0,
+   COMPRIMENTO number,
+   ATIVO char(1) DEFAULT 'S' not null,
+   CONSTRAINT fk_produto
+   FOREIGN KEY (IDPRODUTO)
+   REFERENCES PRODUTO(IDPRODUTO)
+
+);
+
+ALTER TABLE EMBALAGEM ADD CONSTRAINT embalagem_pk PRIMARY KEY (IDPRODUTO, BARRA);
+
+ 
+
+
+-- drop table EXAME_ITEMNF; 
+-- drop table EXAME_NF;
+
+-- Parte 3 – PL/SQL Prático
+-- Exercício 1.
+
+create table EXAME_NF (
+    IDNF number not null constraint EXAME_NF_PK primary key,
+    NUMERO number DEFAULT 0 not null,
+    DATACADASTRO DATE not null,
+    TOTALGERAL number DEFAULT 0 not null
+);
+
+
+-- select * from EXAME_NF
+
+CREATE SEQUENCE EXAME_NF_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER EXAME_NF_bir BEFORE INSERT ON EXAME_NF FOR EACH ROW
+BEGIN
+  SELECT EXAME_NF_seq.NEXTVAL
+  INTO   :new.IDNF
+  FROM   dual;
+END;
+/
+
+
+create table EXAME_ITEMNF (
+    IDITEMNF number not null constraint EXAME_ITEMNF_PK primary key,
+    IDNF number not null,
+    IDPRODUTO number not null,
+    NUMERO number DEFAULT 0 not null,
+    QTDE number DEFAULT 0 not null,
+    VALOR number DEFAULT 0 not null,
+    CONSTRAINT fk_EXAME_ITEMNF_IDITEMNF
+        FOREIGN KEY (IDNF)
+            REFERENCES EXAME_NF(IDNF)
+    -- CONSTRAINT fk_EXAME_ITEMNF_IDPRODUTO
+    --    FOREIGN KEY (IDPRODUTO)
+    --            REFERENCES PRODUTO(IDPRODUTO)                    
+
+);
+
+-- select * from EXAME_ITEMNF
+
+CREATE SEQUENCE EXAME_ITEMNF_seq START WITH 1;
+
+CREATE OR REPLACE TRIGGER EXAME_ITEMNF_bir BEFORE INSERT ON EXAME_ITEMNF FOR EACH ROW
+BEGIN
+  SELECT EXAME_ITEMNF_seq.NEXTVAL
+  INTO   :new.IDITEMNF
+  FROM   dual;
+END;
+/
---
+++ SCRIPT2.sql
@@ -0,0 +1,66 @@
+-- Folha de respostas
+-- Parte 3 – PL/SQL Prático
+-- Nome: Bruno Vieira Schettini da Silva
+-- Data: 11/03/2019
+-- Exercício 2.
+-- Crie um segundo script (SCRIPT2.SQL) que possua um bloco de código para que gere registros fictícios para as tabelas criadas. Gere 1000 registros para a tabela EXAME_NF. Para cada EXAME_NF gere 3 registros. Faça com que a DATACADASTRO do EXAME_NF comece em 10 dias atrás, fazendo com que a cada 100 registros a data seja aumentada em 1 dia, distribuindo assim os 1000 registros em 10 dias diferentes de cadastro.
+
+
+DECLARE 
+
+amount NUMBER := 0;
+valor NUMBER := 0;
+DATACAD DATE := SYSDATE - 10;
+x NUMBER := 0;
+idnf NUMBER := 0;
+itensx NUMBER := 0;
+
+BEGIN
+   FOR xcount IN 1..1000 LOOP
+    
+        IF x = 100  THEN 
+            DATACAD := DATACAD + 1;
+            x := 1;
+            
+        ELSE
+            
+            x := x + 1;
+            
+        END IF;
+        
+   -- SELECT 0 as vdsd INTO amount FROM dual;
+
+        -- EXAME_NF 
+        INSERT INTO EXAME_NF (IDNF, NUMERO, DATACADASTRO, TOTALGERAL) VALUES (xcount, xcount, DATACAD, 0);
+
+        -- SELECT MAX(IDNF) INTO :idnf FROM EXAME_NF;
+        
+                         -- SELECT round(dbms_random.value(1,100)) AS  FROM dual;
+                         
+        -- EXAME_ITEMNF 1                         
+        SELECT CAST(round(dbms_random.value(1,100)) AS number) INTO amount FROM dual;
+        SELECT round(dbms_random.value(1,1000), 2) INTO valor FROM dual;
+        INSERT INTO EXAME_ITEMNF (IDNF, IDPRODUTO, NUMERO, QTDE, VALOR) VALUES (xcount, 1, (xcount * (1 + 1000 + amount)) , amount, valor);
+        -- EXAME_ITEMNF 2
+        SELECT CAST(round(dbms_random.value(1,100)) AS number) INTO amount FROM dual;
+        SELECT round(dbms_random.value(1,1000), 2) INTO valor FROM dual;
+        INSERT INTO EXAME_ITEMNF (IDNF, IDPRODUTO, NUMERO, QTDE, VALOR) VALUES (xcount, 1, (xcount * (1 + 1000 + amount)) , amount, valor);
+        -- EXAME_ITEMNF 3
+        SELECT CAST(round(dbms_random.value(1,100)) AS number) INTO amount FROM dual;
+        SELECT round(dbms_random.value(1,1000), 2) INTO valor FROM dual;
+        INSERT INTO EXAME_ITEMNF (IDNF, IDPRODUTO, NUMERO, QTDE, VALOR) VALUES (xcount, 1, (xcount * (1 + 1000 + amount)) , amount, valor);
+        
+
+        -- Adicionando itens
+        -- FOR itensx IN 1..3 LOOP
+
+        --          SELECT round(dbms_random.value(1,100)) AS  FROM dual;
+
+        --         INSERT INTO EXAME_ITEMNF (xcount, IDPRODUTO, NUMERO, QTDE, VALOR)
+        --         VALUES (xcount, IDPRODUTO, (xcount + itensx + 1000) , amount, VALOR);
+        
+        --    END LOOP;
+            
+    END LOOP;
+END;
+ 
