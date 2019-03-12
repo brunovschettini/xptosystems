2c53bdc0ca8237a6d77bfd56999eaf3186674646
Bruno Schettini <brunovschettini@outlook.com>
2019-03-12 16:11:55 -0300

    Scripts SQL

---
+++ SCRIPT3.sql
@@ -0,0 +1,10 @@
+-- Folha de respostas
+-- Nome: Bruno Vieira Schettini da Silva
+-- Data: 11/03/2019
+-- Parte 3 – PL/SQL Prático
+-- Exercício 3.
+-- Crie uma procedure chamada DEFINIR_VALORES para que altere o valor dos itens da tabela EXAME_ITEMNF, definindo números inteiros aleatórios entre 1 e 100. Faça com que a linha da tabela EXAME_NF receba o valor da somatória de seus itens na coluna TOTALGERAL. Salve o fonte do script no arquivo SCRIPT3.SQL.
+
+
+SELECT
+FROM
---
+++ SCRIPT5.sql
@@ -0,0 +1,6 @@
+-- Folha de respostas
+-- Nome: Bruno Vieira Schettini da Silva
+-- Data: 11/03/2019
+-- Parte 3 – PL/SQL Prático
+-- Exercício 5.
+-- Otimize a última consulta SELECT (Exercício 4), medindo o custo e melhore caso seja possível. Crie índices caso necessário. Salve todas alterações, caso existam, no arquivo SCRIPT5.SQL
